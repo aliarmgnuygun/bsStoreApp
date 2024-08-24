d05e2d044ef07d1b83d76c78efdbcf13a3cc9479
aliarmaganuygun <aau@eskisehir.edu.tr>
2024-08-24 17:18:35 +0300

    ConfigureExceptionHandler has been done.

--- Entities/ErrorModel/ErrorDetails.cs
+++ Entities/ErrorModel/ErrorDetails.cs
@@ -7,9 +7,5 @@ namespace Entities.ErrorModel
         public int StatusCode { get; set; }
         public string? Message { get; set; }
 
-        public override string ToString()
-        {
-            return JsonSerializer.Serialize(this);
-        }
     }
 }
