96b5b32335c71b16c80ded8db1a9a815c6e70be5
aliarmaganuygun <aau@eskisehir.edu.tr>
2024-08-24 13:44:54 +0300

    Configuration has been done for NLog.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,4 @@
+using NLog;
 using WebApi.Extensions;
 
 namespace WebApi
@@ -10,6 +11,7 @@ namespace WebApi
 
             // Add services to the container.
 
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             builder.Services.AddControllers()
                 .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
                 .AddNewtonsoftJson();
@@ -18,6 +20,7 @@ namespace WebApi
             builder.Services.ConfigureSqlContext(builder.Configuration);
             builder.Services.ConfigureRepositoryManager();
             builder.Services.ConfigureServiceManager();
+            builder.Services.ConfigureLoggerService();
 
             var app = builder.Build();
 
